43879f038b6dbc4cb558300d2359fd2ce1138650
Murat <mkoptur3@gmail.com>
2022-07-29 21:48:07 +0300

    file writer test

--- src/file_writer.rs
+++ src/file_writer.rs
@@ -22,10 +22,42 @@ impl FileWriter {
             }
             ResultType::DescribeMany(d) => {
                 let mut file = File::create(&self.fname).unwrap();
-                for (_, v) in d {
+                for v in d.values() {
                     writeln!(file, "{}", v).unwrap();
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{fs, path};
+
+    #[test]
+    fn test_write_file() {
+        let fname = "./examples/iris.csv";
+        let df = super::DataFrame::read(fname).unwrap();
+        let res = df.describe();
+        let res_fname = "./iris_describe.txt";
+        let file_writer = super::FileWriter {
+            fname: res_fname.to_string(),
+            data: super::ResultType::DescribeMany(res),
+        };
+        file_writer.write_file();
+        let res_read = fs::read_to_string(path::Path::new(&res_fname)).unwrap();
+        assert!(res_read.contains("sepal.length"));
+        assert!(res_read.contains("sepal.width"));
+        assert!(res_read.contains("petal.length"));
+        assert!(res_read.contains("petal.width"));
+        assert!(res_read.contains("variety"));
+        assert!(res_read.contains("Unique Count: 3"));
+        assert!(res_read.contains("Unique Values: [\"Setosa\", \"Versicolor\", \"Virginica\"]"));
+        assert!(res_read.contains("Most Freq Value: Setosa"));
+        assert!(res_read.contains("Most Freq Count: 50"));
+        assert!(res_read.contains("Null Count: 0"));
+        assert!(res_read.contains("Min: 0.1"));
+        assert!(res_read.contains("Max: 7.9"));
+        assert!(res_read.contains("Mean: 3.7580001"));
     }
 }
